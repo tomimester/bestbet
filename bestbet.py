ae2b82c0d87288eb43c94aa26243829fd3936e00
Tomi Mester <tomi@data36.com>
2020-09-02 12:51:35 +0000

    new updates, fixed some bugs, started to work on the results screen

--- bestbet.py
+++ bestbet.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 app = Flask(__name__)
 app.secret_key = 'to_be_changed'
 import numpy as np
+import random
 
 def luck_of_the_draw():
     multiplier = 0
@@ -33,19 +34,24 @@ def luck_of_the_draw():
 
 @app.route("/")
 def welcome():
+	session_id = random.randint(100000000000000,(1000000000000000-1))
 	starting_stack = 10000
 	current_stack = starting_stack
 	roundi = 1
+	log_info = []
 	
 	#draw
 	expected_value, multiplier, chance_of_winning, outcome = luck_of_the_draw()
 
 	#passing values
-	session["roundi"] = roundi
-	session["multiplier"] = multiplier
+	session["session_id"] = session_id
 	session["current_stack"] = current_stack
-	session["outcome"] = outcome
+	session["roundi"] = roundi
+	session["expected_value"] = expected_value
 	session["chance_of_winning"] = chance_of_winning
+	session["multiplier"] = multiplier
+	session["outcome"] = outcome
+	session["log_info"] = log_info
 
 	#print
 	roundnr = str(roundi)
@@ -54,19 +60,24 @@ def welcome():
 	cow_flask = str(cow)
 	multiplier_flask = str(multiplier)
 
-	return render_template('test.html', 
+	return render_template('game.html', 
 		roundnr=roundnr, 
 		current_stack_flask=current_stack_flask, 
 		cow_flask=cow_flask, 
-		multiplier_flask=multiplier_flask)
+		multiplier_flask=multiplier_flask,
+		session_id=session_id,
+		log_info=log_info)
 
 @app.route("/", methods=['POST'])
 def game():
-	roundi = session["roundi"]
-	multiplier = session["multiplier"]
+	session_id = session["session_id"]
 	current_stack = session["current_stack"]
-	outcome = session["outcome"]
+	roundi = session["roundi"]
+	expected_value = session["expected_value"]
 	chance_of_winning = session["chance_of_winning"]
+	multiplier = session["multiplier"]
+	outcome = session["outcome"]
+	log_info = session["log_info"]
 
 	risked_money = request.form['text']
 	
@@ -86,28 +97,43 @@ def game():
 		message = "YOU CANT RISK NEGATIVE MONEY, MAN"
 		color_code = 'red'
 	else:
-		roundi = roundi + 1
 		risked_money = int(risked_money)
 		current_stack = current_stack - risked_money
 
 		result = risked_money * multiplier * outcome
 		result = int(result)
 		current_stack = current_stack + result
-		if result == 0:
+		if risked_money == 0:
+			message = "You didn't risk anything."
+			color_code = 'grey'
+		elif result == 0:
 			message = 'Woops, you lost!'
 			color_code = 'red'
 		else:
 			message = 'Yay, you won!'
 			color_code = 'green'
 
-	#draw
+		#logging the values
+		log_info.append([roundi, risked_money, "{:.2f}".format(expected_value), multiplier, "{:.0f}".format(chance_of_winning * 100), outcome, result, current_stack])
+
+		#new round starts
+		roundi = roundi + 1
+
+		#draw
 		expected_value, multiplier, chance_of_winning, outcome = luck_of_the_draw()
 
 	#passing values
-	session["roundi"] = roundi
-	session["multiplier"] = multiplier
+	session["session_id"] = session_id
 	session["current_stack"] = current_stack
+	session["roundi"] = roundi
+	session["expected_value"] = expected_value
+	session["chance_of_winning"] = chance_of_winning
+	session["multiplier"] = multiplier
 	session["outcome"] = outcome
+	session["log_info"] = log_info
+
+	if roundi > 10:
+		return redirect("/result/", code=302)
 
 	#print
 	roundnr = str(roundi)
@@ -116,14 +142,33 @@ def game():
 	cow_flask = str(cow)
 	multiplier_flask = str(multiplier)
 	message_flask = str(message)
-	return render_template('test.html',
+	return render_template('game.html',
 		message_flask=message_flask, 
 		roundnr=roundnr, 
 		current_stack_flask=current_stack_flask, 
 		cow_flask=cow_flask, 
 		multiplier_flask=multiplier_flask, 
-		color_code=color_code)
+		color_code=color_code,
+		session_id=session_id,
+		log_info=log_info)
 
+@app.route("/result/")
+def result():
+	session_id = session["session_id"]
+	current_stack = session["current_stack"]
+	roundi = session["roundi"]
+	expected_value = session["expected_value"]
+	chance_of_winning = session["chance_of_winning"]
+	multiplier = session["multiplier"]
+	outcome = session["outcome"]
+	log_info = session["log_info"]
+
+	current_stack_flask = str(current_stack)
+
+	return render_template('result.html',
+		current_stack_flask=current_stack_flask, 
+		session_id=session_id,
+		log_info=log_info)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
